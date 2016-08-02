3efa2191a616693872a5d3a6ff998edd75333e65
aadamsx <aadams99@gmail.com>
2016-08-02 11:41:20 -0500

    updated the architecture command

--- build-remote.sh
+++ build-remote.sh
@@ -1,4 +1,4 @@
 cd ../meteor
-meteor build ../build --architecture os.linux.x86_64
+meteor build --architecture=os.linux.x86_64 ../build
 scp ../build/meteor.tar.gz user@example.org:/home/meteor/build
 ssh user@example "cd /home/meteor/scripts && ./deploy-build.sh" &
