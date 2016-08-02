61b770e2aa8753ed7b9f0329d8522a456e722f6c
Ramez Rafla <ramez@zegenie.com>
2016-08-02 09:14:03 -0400

    Update build-remote.sh

--- build-remote.sh
+++ build-remote.sh
@@ -1,4 +1,4 @@
 cd ../meteor
-meteor build ../build/meteor --architecture os.linux.x86_64
-scp ../build/meteor/meteor.tar.gz user@example.org:/home/meteor/build
+meteor build ../build --architecture os.linux.x86_64
+scp ../build/meteor.tar.gz user@example.org:/home/meteor/build
 ssh user@example "cd /home/meteor/scripts && ./deploy-build.sh" &
